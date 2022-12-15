f7f04d8599c737cdad6666403a873f8fca5473c1
Deanjer <220509@student.glu.nl>
2022-12-15 11:51:07 +0100

    module4_workshop2

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+document.getElementById("c").onclick = function(){ // als op de c-toets wordt geklikt
+	var c_toets = document.getElementById("audio_c"); // zoek het c-toets audio-element op
+	c_toets.play(); // speel het audio-element af
+}
+document.getElementById("d").onclick = function(){
+	var d_toets = document.getElementById("audio_d");
+	d_toets.play();
+}
+document.getElementById("e").onclick = function(){
+	var e_toets = document.getElementById("audio_e");
+	e_toets.play();
+}
+document.getElementById("f").onclick = function(){
+	var e_toets = document.getElementById("audio_f");
+	e_toets.play();
+}
+document.getElementById("g").onclick = function(){
+	var e_toets = document.getElementById("audio_g");
+	e_toets.play();
+}
+document.getElementById("h").onclick = function(){
+	var e_toets = document.getElementById("audio_h");
+	e_toets.play();
+}
+document.getElementById("i").onclick = function(){
+	var e_toets = document.getElementById("audio_i");
+	e_toets.play();
+}
+document.getElementById("j").onclick = function(){
+	var e_toets = document.getElementById("audio_j");
+	e_toets.play();
+}
+document.getElementById("k").onclick = function(){
+	var e_toets = document.getElementById("audio_k");
+	e_toets.play();
+}
+document.getElementById("m").onclick = function(){
+	var e_toets = document.getElementById("audio_m");
+	e_toets.play();
+}
